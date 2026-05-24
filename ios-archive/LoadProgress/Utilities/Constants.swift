3f9f8f9ce120287569b24d1da1e579950976a95f
Nitish Malpotra <nitishmalpotra@hotmail.com>
2026-05-24 22:35:46 +0100

    remove archive

--- ios-archive/LoadProgress/Utilities/Constants.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-enum Constants {
-    enum Animation {
-        static let standard = SwiftUI.Animation.easeInOut(duration: 0.3)
-    }
-} 
--- ios-archive/LoadProgress/Utilities/NotificationNames.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension Notification.Name {
-    static let workoutDataChanged = Notification.Name("workoutDataChanged")
-    static let newPersonalRecord = Notification.Name("newPersonalRecord")
-}
